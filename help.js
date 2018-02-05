cc3261f37ecaddfe4d2f873eec8bca3c60bfa7cf
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-02-05 14:36:25 -0500

    Corrected Oprah moving outside of canvas

--- help.js
+++ help.js
@@ -4,9 +4,7 @@ var ctx = canvas.getContext('2d');
 // oprah character
 var oprah = {
   x: 210,
-  y: 300,
-  moveLeft:  function() { this.x -= 25 },
-  moveRight: function() { this.x += 25 },
+  y: 300
 }
 
 function draw(oprah) {
@@ -15,20 +13,43 @@ function draw(oprah) {
      ctx.drawImage(img, oprah.x, oprah.y, 155, 100); 
   }
   img.src = "./images/Oprah_main_photo.png";
+
+  // params for width of canvas
+  if (oprah.x <= 0) {
+    oprah.x = (canvas.width + oprah.x);
+  }
+  if (oprah.x >= canvas.width) {
+    oprah.x = (canvas.width - oprah.x);
+  }
 }
+
+// money
+
+
 
 //control oprah
 document.onkeydown = function(e) {
   switch (e.keyCode) {
-    case 37: oprah.moveLeft();  console.log('left',  oprah); break;
-    case 39: oprah.moveRight(); console.log('right', oprah); break;
+    case 37: 
+      // move left  
+      oprah.x -= 25;
+      console.log('left',  oprah); 
+      break;
+    case 39: 
+      // move right 
+      console.log('right', oprah); 
+      oprah.x += 25;
+      break;
   }
   updateCanvas();
 }
+
 
 function updateCanvas() {
   ctx.clearRect(0,0,1500,1700);
   draw(oprah)
 }
 
-updateCanvas()
+updateCanvas();
+
+// add falling money
